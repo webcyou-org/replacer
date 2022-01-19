3e71e1b874ab141204d68561bce64a8c65d79d60
Daisuke Takayama <takayama_daisuke@cocone.co.jp>
2022-01-19 19:15:20 +0900

    refactor: 💡 delete test

--- test/replacer.test.ts
+++ test/replacer.test.ts
@@ -1,14 +1,8 @@
-import DummyClass from "../src/replacer"
-
 /**
  * Dummy test
  */
-describe("Dummy test", () => {
-  it("works if true is truthy", () => {
-    expect(true).toBeTruthy()
-  })
-
-  it("DummyClass is instantiable", () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
-  })
+describe('Dummy test', () => {
+    it('works if true is truthy', () => {
+        expect(true).toBeTruthy()
+    })
 })
